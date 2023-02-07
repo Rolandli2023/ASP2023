ead48ad10f9ed258a815589e7fa9f68b458c6aba
roland <liroland@hotmail.com>
2023-02-06 22:17:32 -0500

    5

---
+++ day5/Cstday5/Cstdemo1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cstdemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creat object from class Person
+           Person P1 = new Person();
+            P1._name = "Tom";
+            P1._gender = "male" ;
+            P1._age = 28;
+            P1.M1();
+            Console.WriteLine("this is {0},gender {1},I am {2}", P1._name, P1._gender, P1._age);
+
+            Console.ReadKey();
+            //Person.M2();
+            Person P2 = new Person();
+            P2._name = "Alice";
+            P2._gender = "Female";
+            P2._age = -100;
+            Console.WriteLine(" this is {0},gender\t {1}\t, I am {2}",P2._name,P2._gender,P2._age);
+            Console.ReadKey();
+
+            Person P3 = new Person();
+            P3._name = "Bob";
+            P3._gender = "Lion";
+            P3._age = 200;
+            Console.WriteLine(" this is {0},gender\t {1}\t, I am {2}", P3._name, P3._gender, P3._age);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ day5/cstdemo2/cstdemo2/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cstdemo2
+{
+    public class Person
+    {
+        public string _name;  //class person,one field(_name)
+        public string _gender;
+        public int _age;
+
+        public void M1()
+        {
+            Console.WriteLine("this is not static method!");
+        }
+        public void M2()
+        {
+            Console.WriteLine("this staic method of person of the person");
+        }
+
+
+
+    }
+}
